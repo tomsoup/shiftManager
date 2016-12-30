fe3f104d7d0d78b967c6c16efbaad82b3cc9027f
Tom Ka Chun Cheung <tomhk1123@gmail.com>
2016-12-29 23:38:18 -0500

    removed photo template"

---
+++ src/components/common/Confirm.js
@@ -0,0 +1,55 @@
+// Import libraries for making component
+import React from 'react';
+import { View, Modal, Text } from 'react-native';
+import { CardSection } from './CardSection';
+import { Button } from './Button';
+
+//props is passed from AlbumDetail
+//Any passed compontent from another component will always be prop.children
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  const { containerStyle, textStyle, cardSectionStyle } = styles;
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType="slide"
+      onRequestClose={() => {}}
+    >
+      <View style={containerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>
+            {children}
+          </Text>
+        </CardSection>
+        <CardSection>
+          <Button onPress={onAccept}>
+            Yes
+          </Button>
+        <Button onPress={onDecline}>
+            No
+          </Button>
+        </CardSection>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = {
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
+};
+
+export { Confirm };
